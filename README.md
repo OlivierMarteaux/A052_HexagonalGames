ff2d7b527627c24635f9372c4115acc828ed7ac5
Olivier Marteaux <olivier_marteaux@hotmail.com>
2025-09-30 15:33:12 +0200

    00_20250930_OM: initial commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# 📱 Hexagonal Games - Players application
+
+
+
+## 🚀 About the Project
+
+
+
+## ✨ Features
+
+
+## 🧰 Tech Stack
+
+
+
+## 📂 Project Structure
+
+
+
+## 📸 Screenshots
+
+
+
+## 📲 Install
+
+
+
+## ⚙️ Setup
+
+
+
+## 👨‍💼 Author
+
+
+
+## 🤝 Acknowledgments
+
+
+
+## 📄 License
+
+This project is for educational and demonstration purposes. Not licensed for commercial use. For inquiries, please contact me.
+
+---
---
+++ app/build.gradle.kts
@@ -0,0 +1,74 @@
+plugins {
+  alias(libs.plugins.androidApplication)
+  alias(libs.plugins.kotlin)
+  alias(libs.plugins.ksp)
+  alias(libs.plugins.hilt)
+}
+
+android {
+  namespace = "com.openclassrooms.hexagonal.games"
+  compileSdk = 34
+
+  defaultConfig {
+    applicationId = "com.openclassrooms.hexagonal.games"
+    minSdk = 24
+    targetSdk = 34
+    versionCode = 1
+    versionName = "1.0"
+
+    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+  }
+
+  buildTypes {
+    release {
+      isMinifyEnabled = false
+      proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+    }
+  }
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+  }
+  composeOptions {
+    kotlinCompilerExtensionVersion = "1.5.11"
+  }
+  kotlinOptions {
+    jvmTarget = "1.8"
+  }
+  buildFeatures {
+    compose = true
+  }
+}
+
+dependencies {
+  //kotlin
+  implementation(platform(libs.kotlin.bom))
+
+  //DI
+  implementation(libs.hilt)
+  ksp(libs.hilt.compiler)
+  implementation(libs.hilt.navigation.compose)
+
+  //compose
+  implementation(platform(libs.compose.bom))
+  implementation(libs.compose.ui)
+  implementation(libs.compose.ui.graphics)
+  implementation(libs.compose.ui.tooling.preview)
+  implementation(libs.material)
+  implementation(libs.compose.material3)
+  implementation(libs.lifecycle.runtime.compose)
+  debugImplementation(libs.compose.ui.tooling)
+  debugImplementation(libs.compose.ui.test.manifest)
+
+  implementation(libs.activity.compose)
+  implementation(libs.navigation.compose)
+  
+  implementation(libs.kotlinx.coroutines.android)
+  
+  implementation(libs.coil.compose)
+  implementation(libs.accompanist.permissions)
+
+  testImplementation(libs.junit)
+  androidTestImplementation(libs.ext.junit)
+  androidTestImplementation(libs.espresso.core)
+}
