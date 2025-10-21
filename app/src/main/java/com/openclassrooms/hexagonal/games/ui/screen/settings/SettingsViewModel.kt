423b416d9acd005aff77c2dabe833e590be1b1ad
Olivier Marteaux <olivier_marteaux@hotmail.com>
2025-10-21 13:50:00 +0200

    tested SettingsViewModel

--- app/src/main/java/com/openclassrooms/hexagonal/games/ui/screen/settings/SettingsViewModel.kt
+++ app/src/main/java/com/openclassrooms/hexagonal/games/ui/screen/settings/SettingsViewModel.kt
@@ -1,20 +1,13 @@
 package com.openclassrooms.hexagonal.games.ui.screen.settings
 
-import android.os.Build
-import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.firebase.ui.auth.AuthUI.getApplicationContext
-import com.google.accompanist.permissions.ExperimentalPermissionsApi
-import com.google.accompanist.permissions.PermissionState
-import com.google.accompanist.permissions.isGranted
-import com.oliviermarteaux.shared.utils.checkNotificationPermission
+import com.oliviermarteaux.localShared.utils.Logger
 import com.openclassrooms.hexagonal.games.data.repository.UserPreferencesRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
-import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -24,57 +17,32 @@ import javax.inject.Inject
  */
 @HiltViewModel
 class SettingsViewModel @Inject constructor(
-  private val userPreferencesRepository: UserPreferencesRepository
+  private val userPreferencesRepository: UserPreferencesRepository,
+  private val log: Logger
 ) : ViewModel() {
   var notifPermissionAlertDialog: Boolean by mutableStateOf(false)
     private set
-
   var notifStateToast: Boolean by mutableStateOf(false)
     private set
-
   var notifState: String by mutableStateOf("disabled")
     private set
-
   fun showNotifStateToast() {
-    Log.d("OM_TAG", "SettingsViewModel: showNotifStateToast()")
+    log.d("SettingsViewModel: showNotifStateToast()")
     viewModelScope.launch {
       notifStateToast = true
       delay(3000)
       notifStateToast = false
     }
   }
-
-//  fun showNotifPermissionAlertDialog(value: Boolean) {
-//    notifPermissionAlertDialog = value
-//  }
   fun toggleNotifications(isNotifEnabled: Boolean) {
     viewModelScope.launch {
       userPreferencesRepository.saveNotificationPreference(isNotifEnabled)
       notifState = if (isNotifEnabled) "enabled" else "disabled"
-      Log.d(
-        "OM_TAG",
-        "SettingsViewModel: toggleNotifications(): $isNotifEnabled"
-      )
+      log.d("SettingsViewModel: toggleNotifications(): $isNotifEnabled")
     }
   }
-
   fun showNotifPermissionAlertDialog(value: Boolean) {
-    Log.d("OM_TAG", "SettingsViewModel: showNotifPermissionAlertDialog($value)")
+    log.d("SettingsViewModel: showNotifPermissionAlertDialog($value)")
     notifPermissionAlertDialog = value
   }
-
-//  @OptIn(ExperimentalPermissionsApi::class)
-//  fun checkNotifPermission(
-//    notifPermissionState: PermissionState?,
-//    onNotifPermissionGranted: () -> Unit
-//  )
-//  {
-//    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-//      if (notifPermissionState?.status?.isGranted == false) {
-//        notifPermissionState.launchPermissionRequest()
-//      } else {
-//        onNotifPermissionGranted()
-//      }
-//    } else { onNotifPermissionGranted() }
-//  }
 }
---
+++ app/src/test/java/com/openclassrooms/hexagonal/games/ui/screen/SettingsViewModelTests.kt
@@ -0,0 +1,111 @@
+package com.openclassrooms.hexagonal.games.ui.screen
+
+import com.oliviermarteaux.localShared.utils.Logger
+import com.oliviermarteaux.localShared.utils.NoOpLogger
+import com.oliviermarteaux.shared.test.assertFlagSwitching
+import com.openclassrooms.hexagonal.games.MainDispatcherRule
+import com.openclassrooms.hexagonal.games.data.repository.UserPreferencesRepository
+import com.openclassrooms.hexagonal.games.ui.screen.settings.SettingsViewModel
+import io.mockk.Runs
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.just
+import io.mockk.mockk
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.advanceUntilIdle
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class SettingsViewModelTest {
+    @get:Rule
+    val mainDispatcherRule = MainDispatcherRule() // handles Dispatchers.Main for tests
+    private val userPreferencesRepository: UserPreferencesRepository = mockk()
+    private val log: Logger = NoOpLogger
+    private lateinit var settingsViewModel: SettingsViewModel
+
+    @Before
+    fun setUp() {
+        settingsViewModel = SettingsViewModel(
+            userPreferencesRepository = userPreferencesRepository,
+            log = log
+        )
+    }
+
+    //_ -------------------------------------------------
+    // showNotifStateToast
+    // -------------------------------------------------
+    @Test
+    fun showNotifStateToast_onCall_ToastFlagToggles() = runTest {
+        // Given
+        assertFalse(settingsViewModel.notifStateToast)
+        // When
+        settingsViewModel.showNotifStateToast()
+        // Then
+        assertFlagSwitching { settingsViewModel.notifStateToast }
+    }
+
+    //_ ------------------------------------------------------------------------
+    // toggleNotifications enables
+    // ------------------------------------------------------------------------
+    @Test
+    fun toggleNotifications_Enable_NotifStateEnabledAndSaved() = runTest {
+        // Given
+        coEvery { userPreferencesRepository.saveNotificationPreference(true) } just Runs
+        // When
+        settingsViewModel.toggleNotifications(true)
+        advanceUntilIdle()
+        // Then
+        coVerify { userPreferencesRepository.saveNotificationPreference(true) }
+        assertEquals("enabled", settingsViewModel.notifState)
+    }
+
+    //_ ------------------------------------------------------------------------
+    // toggleNotifications disables
+    // ------------------------------------------------------------------------
+    @Test
+    fun toggleNotifications_Disable_NotifStateDisabledAndSaved() = runTest {
+        // Given
+        coEvery { userPreferencesRepository.saveNotificationPreference(false) } just Runs
+        // When
+        settingsViewModel.toggleNotifications(false)
+        advanceUntilIdle()
+        // Then
+        coVerify { userPreferencesRepository.saveNotificationPreference(false) }
+        assertEquals("disabled", settingsViewModel.notifState)
+    }
+    //_ ------------------------------------------------------------------------
+    // showNotifPermissionAlertDialog
+    // ------------------------------------------------------------------------
+    @Test
+    fun showNotifPermissionAlertDialog_SetTrue_AlertDialogFlagTrue() {
+        // When
+        settingsViewModel.showNotifPermissionAlertDialog(true)
+        // Then
+        assertTrue(settingsViewModel.notifPermissionAlertDialog)
+    }
+
+    @Test
+    fun showNotifPermissionAlertDialog_SetFalse_AlertDialogFlagFalse() {
+        // When
+        settingsViewModel.showNotifPermissionAlertDialog(false)
+        // Then
+        assertFalse(settingsViewModel.notifPermissionAlertDialog)
+    }
+
+    //_ ------------------------------------------------------------------------
+    // initial state
+    // ------------------------------------------------------------------------
+    @Test
+    fun settingsViewModel_InitialState_HasDefaults() {
+        // Then
+        assertFalse(settingsViewModel.notifPermissionAlertDialog)
+        assertFalse(settingsViewModel.notifStateToast)
+        assertEquals("disabled", settingsViewModel.notifState)
+    }
+}
