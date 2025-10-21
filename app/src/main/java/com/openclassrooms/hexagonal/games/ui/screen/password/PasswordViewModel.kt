6dc96d4038385a451cb8b3deb548545b50bbc038
Olivier Marteaux <olivier_marteaux@hotmail.com>
2025-10-21 12:45:55 +0200

    tested PasswordViewModel

--- app/src/main/java/com/openclassrooms/hexagonal/games/ui/screen/password/PasswordViewModel.kt
+++ app/src/main/java/com/openclassrooms/hexagonal/games/ui/screen/password/PasswordViewModel.kt
@@ -1,20 +1,15 @@
 package com.openclassrooms.hexagonal.games.ui.screen.password
 
-import android.content.Context
 import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
-import com.google.firebase.auth.FirebaseAuthInvalidUserException
 import com.oliviermarteaux.localShared.utils.Logger
-import com.oliviermarteaux.shared.utils.checkInternetConnection
 import com.openclassrooms.hexagonal.games.data.repository.UserRepository
 import com.openclassrooms.hexagonal.games.ui.screen.AuthUserViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
-import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.launch
@@ -52,11 +47,11 @@ class PasswordViewModel @Inject constructor(
                 when (error) {
                     is FirebaseAuthInvalidCredentialsException,
                     is IllegalArgumentException -> {
-                        Log.e("OM_TAG", "PasswordViewModel: signIn: Invalid credentials: ${ error.message ?: "" }")
+                        log.e("PasswordViewModel: signIn: Invalid credentials: ${ error.message ?: "" }")
                         showIncorrectPasswordToast()
                     }
                     else -> {
-                        Log.e("OM_TAG", "PasswordViewModel: Unknown error: ${error.message}")
+                        log.e("PasswordViewModel: Unknown error: ${error.message}")
                         showUnknownErrorToast()
                     }
                 }
