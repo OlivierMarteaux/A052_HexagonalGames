e135f670ef67c6c76fcc7399dd28618473e69f41
Olivier Marteaux <olivier_marteaux@hotmail.com>
2025-10-28 14:25:21 +0100

    moving shared functions to shared library

--- app/src/main/java/com/openclassrooms/hexagonal/games/ui/screen/detail/DetailScreen.kt
+++ app/src/main/java/com/openclassrooms/hexagonal/games/ui/screen/detail/DetailScreen.kt
@@ -1,6 +1,5 @@
 package com.openclassrooms.hexagonal.games.ui.screen.detail
 
-import android.R.attr.height
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
